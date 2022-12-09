712a5a7d2906d11b2aa4a90f474a71b3b020d992
gong <517441540@qq.com>
2022-12-10 02:04:13 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang='ts'>
+import { ref } from 'vue'
+import { ResizeObserver } from 'resize-observer-vue'
+import WrapVue from './wrap.vue'
+
+interface SizeInfoType {
+  width: number
+  height: number
+  offsetWidth: number
+  offsetHeight: number
+}
+
+const sizeRef = ref<SizeInfoType | null>()
+const onResize = (size: SizeInfoType) => {
+  sizeRef.value = size
+}
+</script>
+
+<template>
+  <div>
+    {{ sizeRef?.width }}
+    {{ sizeRef?.height }}
+
+    <ResizeObserver :on-resize="onResize">
+      <WrapVue />
+    </ResizeObserver>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+</style>
---
+++ src/wrap.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+import { } from 'vue'
+</script>
+
+<template>
+  <div>
+    <div>
+      <textarea id="" class="textarea" name="" cols="30" rows="10" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang='css'>
+.textarea{
+  border: 1px solid red;
+}
+</style>
